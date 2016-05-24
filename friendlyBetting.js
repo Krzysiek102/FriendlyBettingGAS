8822f757174694156e22bdcabfc41c07cb2bd48f
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2016-05-25 00:13:40 +0200

    Get poinst for prognosis

--- friendlyBetting.js
+++ friendlyBetting.js
@@ -4,6 +4,32 @@ function NormaliseResult (result){
     return result.replace(/ /g,'').replace('-',':');
 }
 
-function GetPointsForMatch (result, prognosis, pointsForExactResult, pointsForLooseResult){
-    throw new Error("Not implemented");
+function GetResultType (result){
+    var resultSplitted = result.split(":");
+    var scored = resultSplitted[0];
+    var lost = resultSplitted[1]; 
+    if (scored > lost){
+        return 1;
+    }else if (scored === lost){
+        return 0;
+    }else {
+        return 2;
+    }
+}
+
+function GetPointsForPrognosis (exact, prognosis, exactMatch, looseMatch, noMatch){
+    exact = NormaliseResult(exact);
+    prognosis = NormaliseResult(prognosis);
+    if (exact === prognosis)
+    {
+        return exactMatch;
+    }else{
+        var exactType = GetResultType(exact);
+        var prognosisType = GetResultType(prognosis);
+        if (exactType === prognosisType){
+            return looseMatch;
+        }else{
+            return noMatch;
+        }
+    }
 }
--- friendlyBettingSpecs.js
+++ friendlyBettingSpecs.js
@@ -2,18 +2,90 @@
 'use strict';
 
 describe('NormaliseResult', function() {
-    it(' 1  - 0  should be normalised to 1:0', function() {
-        var normalisedResult = NormaliseResult(" 1  - 0 ");
+    it('1:0  should be normalised to 1:0', function() {
+        var normalisedResult = NormaliseResult("1:0");
         expect(normalisedResult).toBe("1:0");        
-    }); 
+    });
+    it('1 : 0  should be normalised to 1:0', function() {
+        var normalisedResult = NormaliseResult("1 : 0");
+        expect(normalisedResult).toBe("1:0");        
+    });
+    it('1 - 0  should be normalised to 1:0', function() {
+        var normalisedResult = NormaliseResult("1 - 0");
+        expect(normalisedResult).toBe("1:0");        
+    });        
+    it(' 1   - 0  should be normalised to 1:0', function() {
+        var normalisedResult = NormaliseResult(" 1   - 0 ");
+        expect(normalisedResult).toBe("1:0");        
+    });    
+});
+
+describe('GetResultType', function() {
+    it('1:0  result is type 1', function() {
+        var resultType = GetResultType("1:0");
+        expect(resultType).toBe(1);
+    });
+    it('1:1  result is type 0', function() {
+        var resultType = GetResultType("0:0");
+        expect(resultType).toBe(0);
+    });
+    it('0:1  result is type 2', function() {
+        var resultType = GetResultType("0:1");
+        expect(resultType).toBe(2);
+    });                
 });
     
 
-
-describe('GetPointsForMatch', function() {
+describe('GetPointsForPrognosis', function() {
     it('1:0 result and 1:0 expect should give 3 points', function() {
-        var points = GetPointsForMatch("1:0", "1:0", 3, 1);
+        var points = GetPointsForPrognosis("1:0", "1:0", 3, 1, 0);
         expect(points).toBe(3);        
     }); 
+    it('1:0 result and 2:0 expect should give 1 point', function() {
+        var points = GetPointsForPrognosis("1:0", "2:0", 3, 1, 0);
+        expect(points).toBe(1);        
+    });  
+    it('1:0 result and 0:0 expect should give 0 points', function() {
+        var points = GetPointsForPrognosis("1:0", "0:0", 3, 1, 0);
+        expect(points).toBe(0);        
+    });
+    it('1:0 result and 0:1 expect should give 0 points', function() {
+        var points = GetPointsForPrognosis("1:0", "0:1", 3, 1, 0);
+        expect(points).toBe(0);        
+    }); 
+    
+    it('1:1 result and 1:0 expect should give 0 points', function() {
+        var points = GetPointsForPrognosis("1:1", "1:0", 3, 1, 0);
+        expect(points).toBe(0);        
+    }); 
+    it('1:1 result and 1:1 expect should give 3 points', function() {
+        var points = GetPointsForPrognosis("1:1", "1:1", 3, 1, 0);
+        expect(points).toBe(3);        
+    });  
+    it('1:1 result and 2:2 expect should give 1 point', function() {
+        var points = GetPointsForPrognosis("1:1", "2:2", 3, 1, 0);
+        expect(points).toBe(1);        
+    });
+    it('1:1 result and 0:1 expect should give 0 points', function() {
+        var points = GetPointsForPrognosis("1:1", "0:1", 3, 1, 0);
+        expect(points).toBe(0);        
+    });  
+    
+    it('0:1 result and 1:0 expect should give 0 points', function() {
+        var points = GetPointsForPrognosis("0:1", "1:0", 3, 1, 0);
+        expect(points).toBe(0);        
+    }); 
+    it('0:1 result and 1:1 expect should give 0 points', function() {
+        var points = GetPointsForPrognosis("0:1", "1:1", 3, 1, 0);
+        expect(points).toBe(0);        
+    });  
+    it('0:1 result and 0:2 expect should give 1 point', function() {
+        var points = GetPointsForPrognosis("0:1", "0:2", 3, 1, 0);
+        expect(points).toBe(1);        
+    });
+    it('0:1 result and 0:1 expect should give 3 points', function() {
+        var points = GetPointsForPrognosis("0:1", "0:1", 3, 1, 0);
+        expect(points).toBe(3);        
+    });                         
 });
     
